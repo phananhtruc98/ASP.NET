f88f040d8aecc0f8944668b7e0dc85d8ab7574d3
Phan Anh Trúc <anhtrucphanit@gmail.com>
2018-12-22 13:15:32 +0700

    _Layout dùng chung cho Views

---
+++ Create_Admin_Area/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Create_Admin_Area.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Create_Admin_Area/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Create_Admin_Area.MvcApplication" Language="C#" %>
