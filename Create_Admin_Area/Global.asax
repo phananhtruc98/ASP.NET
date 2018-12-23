3ee94b01461ca1043ee40051a9695676d0fcda65
Phan Anh Trúc <anhtrucphanit@gmail.com>
2018-12-23 14:39:48 +0700

    OnlineShop update

--- Create_Admin_Area/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Create_Admin_Area.MvcApplication" Language="C#" %>
---
+++ Models/AccountModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Framework;
+using System.Data.SqlClient;
+using System.Data.Sql;
+namespace Models
+{
+    public class AccountModel
+    {
+        private OnlineShopDbContext  context = null; 
+
+        public AccountModel()
+        {
+            context = new OnlineShopDbContext();
+        }
+        public bool Login (string userName, string passWord)
+        {
+            object[] sqlParams =
+            {
+                new SqlParameter("@UserName", userName),
+                new SqlParameter("@Password", passWord)
+            };
+            var res = context.Database.SqlQuery<bool>("SP_Account_Login @UserName, @Password",sqlParams).SingleOrDefault();
+            return res;
+        }
+    }
+}
